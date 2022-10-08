b8b2932fd1c614e6002667d2a267aa7f9f2bd5f5
Kevin Vandeputte <r0792871@student.thomasmore.be>
2022-10-08 13:45:34 +0200

    edge service basis

--- src/main/java/fact/it/edge_service/controller/EdgeServiceController.java
+++ src/main/java/fact/it/edge_service/controller/EdgeServiceController.java
@@ -1,13 +1,14 @@
 package fact.it.edge_service.controller;
 
 import fact.it.edge_service.model.Question;
+import fact.it.edge_service.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -25,20 +26,63 @@ public class EdgeServiceController {
     @Value("${gameservice.baseurl}")
     private String gameServiceBaseUrl;
 
+    /* GET ALL QUESTIONS */
+    //GET: /questions
     @GetMapping("questions")
     public List<Question> getAllQuestions(){
-        List<Question> returnList = new ArrayList<>();
-
         ResponseEntity<List<Question>> responseEntityQuestions =
                 restTemplate.exchange("http://" + gameServiceBaseUrl + "/question",
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<Question>>() {
                         });
 
-        returnList = responseEntityQuestions.getBody();
+        return responseEntityQuestions.getBody();
+    }
 
-        return  returnList;
+    /* GET SPECIFIC USER */
+    //GET: user/{userID}
+    @GetMapping("user/{userID}")
+    public User getUserById(@PathVariable String userID){
+        User u = restTemplate.getForObject("http://" + userServiceBaseUrl +"/user/{userID}",
+                User.class, userID);
+        return u;
+    }
+
+    /* GETTING ALL SCORES TO DISPLAY HIGHSCORES */
+    //GET: /scores
+    @GetMapping("scores")
+    public List<User> getAllScoresAsc(){
+        ResponseEntity<List<User>> responseEntityUsers =
+                restTemplate.exchange("http://" + userServiceBaseUrl + "/scores",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+                        });
+        return responseEntityUsers.getBody();
+    }
+
+    /* POST NEW USER */
+    //POST: /user
+    @PostMapping("/user")
+    public User createNewUser(@RequestBody User u){
+
+        User user = restTemplate.postForObject("http://" + userServiceBaseUrl + "/users",
+                u, User.class);
+
+        return user;
+    }
+
+    /* UPDATE USER SCORE */
+    //PUT: /user/{userID}
+    @PutMapping("user/{userID}")
+    public User updateScore(@RequestBody User u, @PathVariable String userID){
+
+        ResponseEntity<User> responseEntityUser = restTemplate.exchange("http://" + userServiceBaseUrl + "/users/" + userID,
+                HttpMethod.PUT, new HttpEntity<>(u), User.class);
+
+        return responseEntityUser.getBody();
 
     }
+
+
+
 
 
 }
